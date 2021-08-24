c1c9bf55bcb6003752bb62c011784a43843b567c
muhammeddeniz <muhammed.deniz@bakircay.edu.tr>
2021-08-24 23:04:35 +0300

    feat: base component refactor

--- src/assetComponents/icons/Close.tsx
+++ src/assetComponents/icons/Close.tsx
@@ -7,7 +7,7 @@ function SvgClose(props: SvgProps) {
       width={36}
       height={36}
       viewBox="0 0 48 48"
-      fill="currentColor"
+      fill={props.color}
       //@ts-ignore
       xmlns="http://www.w3.org/2000/svg"
       {...props}
@@ -17,13 +17,13 @@ function SvgClose(props: SvgProps) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4 4 12.954 4 24s8.954 20 20 20z"
-        fill="#fff"
+        fill={props.color}
       />
       <Path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M14.879 16.293a1 1 0 000 1.414l6.364 6.364-6.364 6.364a1 1 0 000 1.414l1.414 1.415a1 1 0 001.414 0l6.364-6.364 6.364 6.364a1 1 0 001.414 0l1.415-1.415a1 1 0 000-1.414l-6.365-6.364 6.364-6.364a1 1 0 000-1.414l-1.414-1.414a1 1 0 00-1.414 0l-6.364 6.364-6.364-6.364a1 1 0 00-1.414 0l-1.414 1.414z"
-        fill="#fff"
+        fill={props.color}
       />
     </Svg>
   );
--- src/assetComponents/icons/index.tsx
+++ src/assetComponents/icons/index.tsx
@@ -1,5 +1,5 @@
-export { default as Close } from './Close'
-export { default as Fail } from './Fail'
-export { default as Question } from './Question'
-export { default as Success } from './Success'
-export { default as Warning } from './Warning'
+export { default as Close } from './Close';
+export { default as Fail } from './Fail';
+export { default as Question } from './Question';
+export { default as Success } from './Success';
+export { default as Warning } from './Warning';
